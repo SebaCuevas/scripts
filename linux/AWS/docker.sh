5e4f6dc9595b7824c119f513ecc3152c8eaef6d8
Sebastian Cuevas <sebastian.cuevas@greatgeek.net>
2024-05-29 15:27:58 -0300

    Adding ec2-user to docker group

--- linux/AWS/docker.sh
+++ linux/AWS/docker.sh
@@ -11,13 +11,20 @@ sudo chkconfig docker on
 #Installation of git cli
 sudo yum install -y git
 
-
-
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 # Fix permissions after download
 sudo chmod +x /usr/local/bin/docker-compose
 # Verify success
 docker-compose version
+
+#Creating Docker group
+sudo groupadd docker
+#Adding ec2-user to the docker group.
+sudo usermod -a -G docker ec2-user
+sudo newgrp docker
+
+echo "The system will be rebooted in 60 seconds..."
+sleep 60
 
 #Reboot instance after installation
 sudo reboot
